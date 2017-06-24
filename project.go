7fb019efecae90633f1ff568bb5ec953a01eee0b
Dom <dominic.dipasquale@gmail.com>
2017-06-24 02:36:43 -0400

    Basic working implementation with a database, most functions return immediately

--- project.go
+++ project.go
@@ -7,6 +7,8 @@ import (
 	"database/sql"
 	"log"
 
+	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
@@ -15,33 +17,84 @@ import (
 Message is a struct
 */
 type Message struct {
-	ID      string `json:"id,omitempty"`
+	ID      int    `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
+	UserID  string `json:"userid,omitempty"`
+	Updoots int    `json:"updoots"`
+}
+
+/*
+Newmessage is a struct
+*/
+type Newmessage struct {
 	Message string `json:"message,omitempty"`
 	UserID  string `json:"userid,omitempty"`
 }
 
+/*
+Database is a struct
+*/
+type Database struct {
+	Key string `json:"key,omitempty"`
+}
+
 // Declare our global variables in
 // place of our database
-var messages []Message
+var db *sql.DB
+var err error
 
 /*
 GetAllMessages is a function
 */
 func GetAllMessages(w http.ResponseWriter, req *http.Request) {
+	rows, err := db.Query("SELECT * FROM messages")
+	// If we experience some kind of error
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Uh oh!"))
+		return
+	}
+
+	var id int
+	var mess string
+	var userid string
+	var ups int
+	// These are the messages we will
+	// be sending back
+	var messages []Message
+	//fmt.Println(rows)
+	for rows.Next() {
+		var message Message
+		rows.Scan(&id, &mess, &userid, &ups)
+
+		message.Message = mess
+		message.Updoots = ups
+		messages = append(messages, message)
+	}
+	// Makes sure the client sees application/json
+	w.WriteHeader(http.StatusOK)
+	// else we should have our rows
 	json.NewEncoder(w).Encode(messages)
+}
+
+/*
+checkError is a function
+*/
+func checkError(err error) {
+	fmt.Println(err.Error())
 }
 
 /*
 GetMessage is a function
 */
 func GetMessage(w http.ResponseWriter, req *http.Request) {
+	return
 	params := mux.Vars(req)
-	for _, item := range messages {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
+	stmt, err := db.Prepare("SELECT * FROM messages where id=?")
+	checkError(err)
+	res, err := stmt.Exec(params["id"])
+	checkError(err)
+	fmt.Println(res)
 	// If we don't find a person with that id
 	// send back a blank object
 	json.NewEncoder(w).Encode(&Message{})
@@ -51,44 +104,75 @@ func GetMessage(w http.ResponseWriter, req *http.Request) {
 CreateMessage is a function
 */
 func CreateMessage(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
+	return
+	var nMessage Newmessage
+	json.NewDecoder(req.Body).Decode(&nMessage)
 	// declare a new message
-	var message Message
-	_ = json.NewDecoder(req.Body).Decode(&message)
-	message.ID = params["id"]
-	messages = append(messages, message)
+	// All new messages will have 0 updoots to start
+	stmt, err := db.Prepare("INSERT messages SET message=?,userid=?,updoots=0")
+	if err != nil {
+		return
+	}
+	res, err := stmt.Exec(nMessage.Message, "Y87YUHG989839RW09U98")
+	checkError(err)
 	// return the newly created object
-	json.NewEncoder(w).Encode(message)
+	json.NewEncoder(w).Encode(res)
 }
 
 /*
 DeleteMessage is a function
 */
 func DeleteMessage(w http.ResponseWriter, req *http.Request) {
+	return
 	params := mux.Vars(req)
-	for index, item := range messages {
-		// loop through until the id matches,
-		// then remove it and break
-		if item.ID == params["id"] {
-			messages = append(messages[:index], messages[index+1:]...)
-			break
-		}
+
+	stmt, err := db.Prepare("DELETE FROM messages WHERE id=?")
+	checkError(err)
+	res, err := stmt.Exec(params["id"])
+	fmt.Println(res)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Uh oh"))
+		return
 	}
-	json.NewEncoder(w).Encode(messages)
+
+	w.WriteHeader(200)
+	w.Write([]byte("Success."))
+}
+
+/*
+CreateTable is a function
+*/
+func CreateTable(w http.ResponseWriter, req *http.Request) {
+	return
+	var keys Database
+	json.NewDecoder(req.Body).Decode(&keys)
+	// This is the key we will use to verify that they have acces
+	// to the database, no point in implementing logins
+	if keys.Key != "WKHEAW33X9ZJ9VAG5VTD" {
+		w.WriteHeader(404)
+		w.Write([]byte("404 page not found"))
+		return
+	}
 }
 
 /*
 main is a function
 */
 func main() {
-	db, err := sql.Open("mysql")
+	// Note that here we must use a strict = rather than :=
+	db, err = sql.Open("mysql", "nuser:npassword@tcp(pittyak_db:3306)/testdb")
+
+	//checkError(err)
+	defer db.Close()
+
 	router := mux.NewRouter()
-	messages = append(messages, Message{ID: "1", Message: "The very first post!"})
 
 	router.HandleFunc("/message", GetAllMessages).Methods("GET")
 	router.HandleFunc("/message/{id}", GetMessage).Methods("GET")
-	router.HandleFunc("/message/{id}", CreateMessage).Methods("POST")
+	router.HandleFunc("/message", CreateMessage).Methods("POST")
 	router.HandleFunc("/message/{id}", DeleteMessage).Methods("DELETE")
+	router.HandleFunc("/admin/create", CreateTable).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":3001", router))
 }
