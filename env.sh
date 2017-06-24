95372b732d29bc91097eeb1923f2b612d69793f7
Dom <dominic.dipasquale@gmail.com>
2017-06-24 02:35:19 -0400

    remove unnecessary files

--- env.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# Set the gopath to our current working directory
-export GOPATH=$(pwd)
-# For convenience, add the workspace's bin subdirectory to your PATH
-export PATH=$PATH:$(go env GOPATH)/bin
