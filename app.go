5960243c7c3f84699f8f12ccab08f882a0e4ef80
dom <dominic.dipasquale@gmail.com>
2017-07-24 21:11:45 -0400

    broken

--- app.go
+++ app.go
@@ -41,7 +41,7 @@ func main() {
 	// Make sure to allow all requests
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:8100", "http://localhost:8080", "http://localhost:3000", "https://updoot.us"},
+		AllowedOrigins: []string{"http://localhost:8100/", "http://localhost:8080", "http://localhost:3000", "https://updoot.us"},
 		AllowCredentials: true,
 		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders: []string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
